3b72f934c23f0a257d82241da7ea1b68636ec133
Arthur Donkovtsev <donkovtsev@icloud.com>
2018-04-10 10:15:03 +0300

    add files

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,22 +1,25 @@
 import React, { Component } from "react";
 
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 
+import FriendListApp from "./FriendListApp";
+import * as reducers from "../reducers";
 
 
 import "../styles/app.css";
 import img from '../images/1.jpg';
 
 
+const reducer = combineReducers(reducers);
+const store = createStore(reducer);
 
 export default class App extends Component {
   render() {
     return <div className="app">
-        <a href=""> My Minimal React 16 Webpack 4 </a>
-        <div>
-          <img src={img} alt={img} />
-        </div>
+        <Provider store={store} >
+          <FriendListApp />
+        </Provider>
       </div>;
   }
 }
