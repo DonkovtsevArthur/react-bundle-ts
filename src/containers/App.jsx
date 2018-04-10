de85ea4994f33c23a2003668108ddabab7f66b92
Arthur Donkovtsev <donkovtsev@icloud.com>
2018-04-10 19:24:34 +0300

    add window.redux_devtools

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -9,7 +9,10 @@ import addFriend from "../reducers";
 
 import "../styles/app.css";
 
-const store = createStore(addFriend);
+const store = createStore(
+  addFriend,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default class App extends Component {
   render() {
